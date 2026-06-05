4b832fb81d4701154f9f2dd8dc37164a1b1c489f
Chuang-Yu Cheng <cycheng2@amd.com>
2026-06-05 06:53:38 +0000

    [stinkytofu] remove unrollMovableBarrier config

--- shared/stinkytofu/src/pipeline/backend/Gfx1250Backend.cpp
+++ shared/stinkytofu/src/pipeline/backend/Gfx1250Backend.cpp
@@ -108,7 +108,6 @@ bool buildGfx1250Pipeline(PassManager& pm, StinkyAsmModule& module) {
         PassFeatureConfig passFeatureConfig;
         std::shared_ptr<DAGScheduleJsonCollector> snapshotCollector;
         if (runScheduler) {
-            passFeatureConfig.barrierConfig.unrollMovableBarrier = true;
             passFeatureConfig.loopConfig.unrollGemm = true;
             passFeatureConfig.dagFeatures.distributeGlobalRead = true;
             passFeatureConfig.passOrderSnapshot.jsonPath = moduleOptions.PassOrderSnapshotJson;
